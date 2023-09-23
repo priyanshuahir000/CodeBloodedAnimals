da5ffa01e361db21f7a2fdf73904b0034ab5c0f2
Ankur Ranpariya <33175753+AnkurRanpariya2005@users.noreply.github.com>
2023-09-23 14:32:24 +0530

    Add files via upload
    Added some files

--- Appoiment/appoiment/clinic/booking/views.py
+++ Appoiment/appoiment/clinic/booking/views.py
@@ -4,7 +4,14 @@ from .models import *
 from django.contrib import messages
 
 def index(request):
-    return render(request, "index.html",{})
+    doctor = Doctor.objects.all()
+    context={
+        'doctor':doctor
+    }
+    return render(request, "index.html",context)
+
+def select_doctor(request):
+    pass
 
 def booking(request):
     #Calling 'validWeekday' Function to Loop days you want in the next 21 days:
--- Appoiment/appoiment/clinic/members/forms.py
+++ Appoiment/appoiment/clinic/members/forms.py
@@ -1,6 +1,7 @@
 from tkinter import Widget
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import CustomUser
 from django import forms
 from django.db import models
 
@@ -11,7 +12,7 @@ class RegisterUserForm(UserCreationForm):
     last_name = forms.CharField(label='Last Name', max_length=50, widget=forms.TextInput(attrs={'class':'form-control form-control-lg'}))
     
     class Meta:
-        model = User
+        model = CustomUser
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
     def __init__(self, *args, **kwargs):
